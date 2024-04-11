28d2e0eaad21093a788e4607b6c16fa6a6470eed
Tanisha-sain <131015973+Tanisha-sain@users.noreply.github.com>
2024-04-11 16:08:11 +0000

    numbers in python

---
+++ 01_basics/numbers.py
@@ -0,0 +1,25 @@
+x = 2
+y = 3
+z = 7
+
+print(x+y)
+# print('Tani' + 5)      TypeError: can only concatenate str (not "int") to str
+
+print('Tani' + 'sha')
+print(x*y)
+print(z%x)
+print(40 + 42.56)
+
+print(float(40))
+print(int(42.56))
+
+print(int(43.56) % int(3.0))
+
+print(100 ** 2)
+print(2 ** 100)
+print(2 ** 1000)
+
+print(repr('chai'))
+print(str('chai'))
+print('chai')
+
